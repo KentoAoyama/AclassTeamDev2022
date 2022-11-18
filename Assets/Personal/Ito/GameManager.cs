08708014e8af1cd915f764e7e48be1692b7772ab
伊藤廣軒 <106290558+ItouHiroken@users.noreply.github.com>
2022-11-18 16:57:39 +0900

    [Add]時間カウントのUIとWaveCenterの数を表示するUIをつくった

--- Assets/Personal/Ito/GameManager.cs
+++ Assets/Personal/Ito/GameManager.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public float WaveCenterCount { get => _waveCenterCount; set => _waveCenterCount = value; }
+    public float WaveCenterCount => _waveCenterCount;
     [SerializeField] private float _waveCenterCount;
-    public float Time { get => _time; set => _time = value; }
-    [SerializeField] private float _time;
+    public float Timer => _timer;//get‚¾‚¯‚ç‚µ‚¢
+    [SerializeField] private float _timer;
+    [SerializeField] GameObject _player;
+
+    private void Start()
+    {
+    
+    }
+    void Update()
+    {
+        TimerCount();
+    }
+    void TimerCount()
+    {
+        _timer += Time.deltaTime;
+    }
+    public void WaveCenterCountPlus()
+    {
+        _waveCenterCount++;
+    }
 }
