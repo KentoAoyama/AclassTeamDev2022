cc43ab00a4ddb10b0c59ec7df64f19edc2a60b48
青山研人 <106291245+KentoAoyama@users.noreply.github.com>
2022-11-20 22:23:37 +0900

    [update]GameOver機能追加

--- Assets/Personal/Aoyama/Scripts/PlayerMove.cs
+++ Assets/Personal/Aoyama/Scripts/PlayerMove.cs
@@ -10,9 +10,6 @@ public class PlayerMove
     /// <summary>
     /// Player‚ÌˆÚ“®‚Ìˆ—
     /// </summary>
-    /// <param name="rb"></param>
-    /// <param name="inputX"></param>
-    /// <param name="InputY"></param>
     public void Move(Rigidbody2D rb, float moveSpeed,float inputX, float InputY)
     {
         rb.velocity = new Vector2(inputX, InputY) * moveSpeed;
